1ac9bef8851d0904c3f9e8d35c540ccd8b006205
Irakli-Beridze28 <200980878+Irakli-Beridze28@users.noreply.github.com>
2025-09-20 16:24:41 +0400

    Add game reset button functionality.

--- script.js
+++ script.js
@@ -56,30 +56,31 @@ function playRound(humanChoice, computerChoice) {
         rockButton.disabled = true;
         paperButton.disabled = true;
         scissorButton.disabled = true;
-        // let humanWin = document.createElement("p");
-        // humanWin.textContent = "You have won the game!";
-        // results.appendChild(humanWin);
     } else if (computerScore >= 5) {
         roundResult.textContent = "You have lost the Game";
         rockButton.disabled = true;
         paperButton.disabled = true;
         scissorButton.disabled = true;
-        // let computerWin = document.createElement("p");
-        // computerWin.textContent = "You have lost the game!";
-        // results.appendChild(computerWin);
-    // } else if (humanScore === computerScore) {
-    //     let draw = document.createElement("P");
-    //     draw.textContent = "It's Draw";
     }
 }
 
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorButton = document.querySelector("#scissors");
+const resetButton = document.querySelector("#game-reset");
 
 rockButton.addEventListener("click", () => playRound(0, getComputerChoice()));
 paperButton.addEventListener("click", () => playRound(1, getComputerChoice()));
 scissorButton.addEventListener("click", () => playRound(2, getComputerChoice()));
+resetButton.addEventListener("click", () => {
+    humanScore = 0;
+    computerScore = 0;
+    roundResult.textContent = "";
+    score.textContent = `Human: ${humanScore} | Computer: ${computerScore}`;
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorButton.disabled = false;
+});
 
 const roundResult = document.querySelector("#round-result");
 const score = document.querySelector("#score");
